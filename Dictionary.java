2beaa8270b49cb6a930f6d1035077e2335299ad3
Yuri Domingos <64822018+YuriDomingos@users.noreply.github.com>
2020-12-26 22:46:18 +0100

    Add files via upload

---
+++ Dictionary.java
@@ -0,0 +1,17 @@
+
+package dictionarytest;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+
+
+public class PropertiesTest extends JFrame {
+    
+  
+
+}
